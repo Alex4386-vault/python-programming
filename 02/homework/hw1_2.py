7001ce5a7273bbafca729c4df24caecc5e0c6537
Alex4386 <me@alex4386.us>
2019-09-09 11:29:17 +0900

    remove: functions

--- 02/homework/hw1_2.py
+++ 02/homework/hw1_2.py
@@ -2,9 +2,6 @@
     Homework 2:
 '''
 
-def killometers2Miles(killometers):
-    return killometers * 0.621371
-
 km = 5.5
 
-print(km,"km is equal to",killometers2Miles(km),"miles")
+print(km,"km is equal to",km * 0.621371,"miles")
