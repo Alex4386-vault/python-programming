bfd67925c901c2d366f611a50b3e3e07e8aea918
Alex4386 <psh010209@gmail.com>
2019-09-09 11:40:05 +0900

    add: total homework file

---
+++ 02/homework/hw1_total.py
@@ -0,0 +1,22 @@
+
+#    Homework 1:
+
+# import math
+# PI = math.pi
+PI = 3.14
+
+radius = 12.5
+Circumference = (2 * radius) * PI
+Area = (radius ** 2) * PI
+
+print("radius =", radius)
+print("Circumference =", Circumference)
+print("Area =", Area)
+
+
+
+#    Homework 2:
+
+km = 5.5
+
+print(km,"km is equal to",km * 0.621371,"miles")
