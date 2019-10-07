270b01727c2587ba9cdd7dd05f1e97649ca3985b
Alex4386 <psh010209@gmail.com>
2019-10-07 13:04:40 +0900

    add: hw

--- 04/homework/parksanghui3-1.py
+++ 04/homework/parksanghui3-1.py
@@ -7,7 +7,9 @@ for i in range(howMany):
 
     profitStr, lossStr, noProfitLoss = "Total Profit = {amount}",  "Total Loss Amount = {amount}", "No Profit No Loss!!!"
 
+    # The homework requires to use if-elif-else chain
     #print((profitStr if (sales - cost) > 0 else noProfitLoss if (sales == cost) else lossStr ).format(amount=abs(round(sales-cost,1))))
+
     a = sales-cost
     if a > 0:
         print(profitStr.format(amount=abs(round(a,1))))
